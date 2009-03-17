567907fb7c8de02c3eae1af06d6a614ccfd47abf
Peter Law <PeterJCLaw@gmail.com>
2009-03-17 21:27:49 +0000

    change the error text to be more kind to the user - pasted from Error.php
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@52 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
 $page_file	= "$data_root/".(empty($page_req) ? "1-Home" : $page_req).".page";
 
 if(!is_readable($page_file)) {
-	echo "The page you requested doesn't exist!\n<br />\nIf an internal link brought you here then please "
-			.email_link("email", 'them', "webmaster", "$website_name_short $page_file failure", 0, 0, $debug_info.$error)
-			." the Web Master to inform them of this.<br />";
-} else {
+//	header('HTTP/1.1 404 Not Found');
+	$email_link = email_link("email", 0, "webmaster","$website_name_short $page_req $id  Error whilst following a link from ".$_SERVER["HTTP_REFERER"],0,0,0);
+?>
+	<div class="center">
+	<p>
+		You are seeing this page because you are in the page you requested does not exist.
+	</p><p>
+		If an internal Link sent you here please <?php echo $email_link; ?>
+		the <?php echo ( isset($Site_TOClist['AliasList']['Committee']) ? '<a href="Committee#webmaster">Webmaster</a>' : 'Webmaster'); ?> to report the broken link.
+	</p><p>
+		Please use the button below or your browser's back button to return to the page you were on.
+	</p><p>
+		Happy browsing!
+	</p><p>
+		<button onclick="javascript:history.go(-1)">Back</button>
+	</div>
+
+<?php } else {
 	echo SquidgyParser($page_file);
 }
 	include 'Foot.inc.php';
