bc3db161cd11e377443e097a514ee9611b2c1b70
Peter Law <PeterJCLaw@gmail.com>
2009-03-18 00:08:19 +0000

    just in case we can't get debug info from the Site block we can get it from here instead, using a different value for debug though
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@62 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Foot.inc.php
+++ Foot.inc.php
@@ -1,1 +1,7 @@
-<?php echo "\n	</div><!-- end content div -->\n".SquidgyParser($template_file, '[[Block::Site-Content]]'); ?>
+<?php
+echo "\n	</div><!-- end content div -->\n".SquidgyParser($template_file, '[[Block::Site-Content]]');
+if($debug < 0) {
+	echo $debug_info;
+	echo show_log();
+}
+?>
