a2ddaa354f3f321f39e73902b8a224135f81e146
Peter Law <PeterJCLaw@gmail.com>
2009-03-21 13:09:44 +0000

    move to switch-case, handle the non-existence of classes better
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@73 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- ajax.php
+++ ajax.php
@@ -11,26 +11,30 @@ require_once("Modules/$class.module.php");
 
 $class_name	= ucwords($type).$class;
 
-if($type == 'admin') {
-	if(class_exists($class_name)) {
-		$thisobj	= new $class_name();
+switch($type) {
+	case 'admin':
+		if(class_exists($class_name)) {
+			$thisobj	= new $class_name();
 
-		$host_arr	= $thisobj->get_info();
-		$host_arr['obj']	= $thisobj;
+			$host_arr	= $thisobj->get_info();
+			$host_arr['obj']	= $thisobj;
 
-		print_Admin_Section($host_arr);
-	}
-} elseif($type == 'block') {
-	if(class_exists($class_name) && method_exists($class_name, 'ajax')) {
-		$thisobj	= new $class_name();
-		echo $thisobj->ajax();
-	} else {
-		echo "Ajax method not defined in block '$class_name'.";
-	}
+			print_Admin_Section($host_arr);
+		} else
+			echo "Admin class not defined in module '$class'.";
+		break;
 	
-} elseif($type == 'preview') {
-} else {
-	echo "Request type not recognised.";
+	case 'block':
+		if(class_exists($class_name) && method_exists($class_name, 'ajax')) {
+			$thisobj	= new $class_name();
+			echo $thisobj->ajax();
+		} else
+			echo "Ajax method not defined in block '$class_name'.";
+		break;
+		
+	case 'preview':
+	default:
+		echo "Request type not recognised.";
 }
 
 if($debug)
