37e48c84cfc3846835aabd70be4f4285de14e16a
Peter Law <PeterJCLaw@gmail.com>
2009-04-26 14:11:36 +0000

    fix the classes on the profile listing
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@170 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Modules/Profile.module.php
+++ Modules/Profile.module.php
@@ -108,16 +108,16 @@ class BlockProfile extends Block {
 			include Users::file($info_name);
 			$firstname = first_name($name);
 			$ret	.= '<tr>
-		<td rowspan="2" class="Profile::get_image">
-			<a href="'.Profile::get_image($image_path, 'orig').'" title="Click image to view larger"><img src="'.Profile::get_image($image_path).'" alt="'."$firstname, $job".'" title="Click image to view larger" /></a>				</td>
-		<td>
-			<h4 id="'.str_replace(".", "_", $info_name).'" class="comm_name">'."$job - ".stripslashes($name).'</h4>
-			<span class="comm_email">
+		<td rowspan="2" class="user_pic">
+			<a href="'.Profile::get_image($image_path, 'orig').'" title="Click image to view larger"><img src="'.Profile::get_image($image_path).'" alt="'."$firstname, $job".'" title="Click image to view larger" /></a>
+		</td><td>
+			<h4 id="'.str_replace(".", "_", $info_name).'" class="user_name">'."$job - ".stripslashes($name).'</h4>
+			<span class="user_email">
 				'.email_link("email $gender", $gender, $info_name, 0,0,0,0).' or use the <a href="Contact_Us.php?target='.$job.'" title="Use the online contact form">contact form</a>.
 			</span>
 		</td>
 	</tr><tr>
-		<td class="comm_txt">
+		<td class="user_txt">
 			'.stripslashes($spiel) //gets rid of any slashes invoked by (')s
 			.'
 		</td>
