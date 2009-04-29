2564bfa193496c6c5d2b10d8a1b8449f6a1fcde0
Peter Law <PeterJCLaw@gmail.com>
2009-04-29 21:49:32 +0000

    since FileSystem now checks that the dir passed is readable (and fails gracefully) we don't need to do that here
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@176 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Global.inc.php
+++ Global.inc.php
@@ -62,13 +62,9 @@ $debug_info		.="\n ini_set('url_rewriter.tags', '') = ".ini_set('url_rewriter.ta
 $referrer		= isset($_SERVER['HTTP_REFERER']) ? htmlspecialchars($_SERVER['HTTP_REFERER']) : "";
 
 if(has_method('FileSystem', "filtered_file_list")) {
-	if(is_readable($data_root)) {	//make the page and article arrays
-		$GEN_pages	= FileSystem::Filtered_File_List($data_root, ".page");
-		$GEN_art	= FileSystem::Filtered_File_List($data_root, ".article");
-	} else {
-		$GEN_pages	= array();
-		$GEN_art	= array();
-	}
+	//make the page and article arrays
+	$GEN_pages	= FileSystem::Filtered_File_List($data_root, ".page");
+	$GEN_art	= FileSystem::Filtered_File_List($data_root, ".article");
 
 	// make a list of committee members whose information is available
 	$debug_info .= "building \$joblist\n<br />\n";
