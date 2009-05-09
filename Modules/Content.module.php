b85ac73995e1244a775ebaccd45af53a4049bac5
Peter Law <PeterJCLaw@gmail.com>
2009-05-09 01:55:01 +0000

    a new Content module, built from the Page module, complete.
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@181 2fba2af6-d892-294d-8f6b-3ba2989aa45f

---
+++ Modules/Content.module.php
@@ -0,0 +1,76 @@
+<?php
+#name = Content
+#description = Allows site content to be created and edited
+#package = Core - required
+#type = content
+###
+
+class AdminContent extends Admin {
+	function AdminContent() {
+		parent::__construct('Create or change content chunks made from html and site blocks', -1);
+	}
+
+	function printFormAdmin() {
+		global $page_req;
+
+		$Chunks = FileSystem::Filtered_File_List($this->data_root, '.chunk');
+		natsort($Chunks);
+
+		if(!empty($page_req) && in_array($page_req, $Chunks)) {
+			$content	= file_get_contents("$this->data_root/$page_req.chunk");
+			$chunk_title	= get_GEN_title($page_req);
+			$chunk_id	= $chunk_req;
+		} else {
+			$content	= $chunk_title	= "";
+			$chunk_id	= "new";
+		}
+		?>
+			<input type="hidden" name="chunk_id" id="chunk_id" value="<?php echo $chunk_id; ?>" />
+			<table><tr>
+				<th><label for="chunk_title" title="The title of the chunk">Chunk Title:</label></th>
+				<td><input  value="<?php echo $chunk_title; ?>" class="text" name="chunk_title" id="chunk_title" title="The title of the page" /></td>
+			</tr><tr>
+				<?php $this->print_select_cells($Chunks, $page_req); ?>
+			</tr></table>
+<?php
+		$this->printTextarea($content);
+		return;
+	}
+
+	function submit()
+	{
+		global $content, $chunk_title, $chunk_id, $header_link, $debug_info;
+
+		$error	= "";
+
+		if(empty($content))
+			$error	.= "\nNo content provided";
+		if(strpos($content, '<?') !== FALSE)
+			$error	.= "\nInvalid content provided: PHP is not allowed";
+		if(empty($chunk_title))
+			$error	.= "\nNo title provided";
+
+		if(!empty($error))	//if there's an error then bail
+			return $error;
+
+		$old_chunk_id	= urldecode($chunk_id);
+
+		if($chunk_id == 'new') {	//if its a new page
+			$chunk_id	= urlencode(get_next_id($this->data_root, ".chunk")."-$chunk_title");
+		} elseif($this->new_id_needed($old_chunk_id, $chunk_title) && is_writable("$this->data_root/$chunk_id.chunk")) {	//if we should and can change stuff
+			$chunk_id	= get_next_id($this->data_root, ".chunk")."-$chunk_title";
+			$error	= $this->change_something_in_all_pages($old_chunk_id, $chunk_id);
+			if(!empty($error))	//if there's an error then bail
+				return $error;
+			$error	= $this->delete_file("$this->data_root/".urlencode($old_chunk_id).".chunk");
+			$chunk_id	= urlencode($chunk_id);
+		}
+
+		$debug_info .= @"\$old_chunk_id = '$old_chunk_id', \$chunk_id = '$chunk_id'\n<br />\n";
+
+		$header_link	= "&p=$chunk_id";
+
+		return FileSystem::file_put_stuff("$this->data_root/".$chunk_id.".chunk", $content, 'w');
+	}
+}
+?>
