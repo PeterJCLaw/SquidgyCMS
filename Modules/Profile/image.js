f3b5691ed258543745378a40991d449bbbed904f
Peter Law <PeterJCLaw@gmail.com>
2009-05-23 02:57:44 +0000

    profile image maker: add a border to the preview box, add the ability to resize the select box and do this such that the preview remains valid\!
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@244 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Modules/Profile/image.js
+++ Modules/Profile/image.js
@@ -23,6 +23,9 @@ function click(e) {
 	originTop = $('selectbox').offsetTop;
 	originLeft = $('selectbox').offsetLeft;
 
+	originHeight = $('grabbox').offsetHeight;
+	originWidth = $('grabbox').offsetWidth;
+
 	if(e.type == 'mousedown')
 		e.originalTarget.onmousemove = move;
 	else
@@ -43,16 +46,42 @@ function move(e) {
 	var out = 'originTop:' + originTop + '<br />top:' + top + '<br />originLeft:' + originLeft + '<br />left:' + left;
 	$('jam').innerHTML = out;
 
-	var prev_top = originTop + top;
-	var prev_left = originLeft + left;
+	var select_top = originTop + top;
+	var select_left = originLeft + left;
 
-	$('selectbox').style.top = prev_top + 'px';
-	$('selectbox').style.left = prev_left + 'px';
+	if(e.originalTarget.id != null && e.originalTarget.id == 'grabbox') {
+		$('selectbox').style.top = select_top + 'px';
+		$('selectbox').style.left = select_left + 'px';
+	} else {
+		var classes = (e.originalTarget.className+' ').split(' ');
+
+		if(classes[0] == 'n' || classes[1] == 'n') {
+			$('selectbox').style.top = select_top + 'px';
+			$('grabbox').style.height = originHeight - top + 'px';
+		}
+
+		if(classes[0] == 'w' || classes[1] == 'w') {
+			$('selectbox').style.left = select_left + 'px';
+			$('grabbox').style.width = originWidth - left + 'px';
+		}
+
+		if(classes[0] == 's' || classes[1] == 's') {
+			$('grabbox').style.height = originHeight + top + 'px';
+			top = 0;
+		}
+
+		if(classes[0] == 'e' || classes[1] == 'e') {
+			$('grabbox').style.width = originWidth + left  + 'px';
+			left = 0;
+		}
+	}
+	var prev_top = originTop + top - 3;
+	var prev_left = originLeft + left - 3;
 
 	refresh_preview(prev_top,prev_left,$('grabbox').offsetHeight,$('grabbox').offsetWidth);
 //*/
 }
 
 add_loader( function() { $('previewbox').style.backgroundImage = 'url('+$('srcImg').src+')'; });
-var _MOUSE_X,_MOUSE_Y,originTop,originLeft;
+var _MOUSE_X,_MOUSE_Y,originTop,originLeft,originHeight,originWidth;
 
