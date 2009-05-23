6bc8c3e688bc42250d6dffbe2a50cb0153f0e4cb
Peter Law <PeterJCLaw@gmail.com>
2009-05-23 01:07:00 +0000

    profile image maker: add a debug box, make the preview box resize itself as scaling seemingly can't be done, make the moving of the select box work
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@243 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Modules/Profile/image.js
+++ Modules/Profile/image.js
@@ -1,33 +1,58 @@
 
-function refresh_preview() {
-	$('previewbox').style.background
+function refresh_preview(top,left,height,width) {
+	$('previewbox').style.backgroundPosition = -1*left + 'px ' + -1*top + 'px';
+	$('previewbox').style.height = height+'px';
+	$('previewbox').style.width = width+'px';
 }
 
-function click(ev) {
-//	console.dir(ev);
-	window._TARGET = ev.originalTarget;
-	window._MOUSE_X = ev.clientX;
-	window._MOUSE_Y = ev.clientY;
+function click(e) {
+	if(!e)
+		e = window.event;
 
-	if(ev.type == 'mousedown')
-		ev.originalTarget.onmousemove = move;
+	e.preventDefault();
+	e.stopPropagation();
+
+	_MOUSE_X = e.pageX;
+	_MOUSE_Y = e.pageY;
+
+	var out = 'spam';
+	out += '<br />pageX:' + _MOUSE_X + '<br />pageY:' + _MOUSE_Y;
+
+	$('debugbox').innerHTML = out+'<br /><span id="jam"></span>';
+
+	originTop = $('selectbox').offsetTop;
+	originLeft = $('selectbox').offsetLeft;
+
+	if(e.type == 'mousedown')
+		e.originalTarget.onmousemove = move;
 	else
-		ev.originalTarget.onmousemove = null;
+		e.originalTarget.onmousemove = null;
+//*/
 }
 
-function move(ev) {
-	console.dir(ev);
-	console.dir(ev.clientX);
-	console.dir(window);
-	console.dir(window._MOUSE_X);
-/*	left = ev.clientX - window._MOUSE_X;
-	top = ev.clientY - window._MOUSE_Y;
-	console.dir(top);
-	console.dir(left);
-	_TARGET.style.marginTop = top+'px';
-	_TARGET.style.marginLeft = left+'px';
-*/
+function move(e) {
+	if(!e)
+		e = window.event;
+
+	e.preventDefault();
+	e.stopPropagation();
+
+	var left = e.pageX - _MOUSE_X;
+	var top = e.pageY - _MOUSE_Y;
+
+	var out = 'originTop:' + originTop + '<br />top:' + top + '<br />originLeft:' + originLeft + '<br />left:' + left;
+	$('jam').innerHTML = out;
+
+	var prev_top = originTop + top;
+	var prev_left = originLeft + left;
+
+	$('selectbox').style.top = prev_top + 'px';
+	$('selectbox').style.left = prev_left + 'px';
+
+	refresh_preview(prev_top,prev_left,$('grabbox').offsetHeight,$('grabbox').offsetWidth);
+//*/
 }
 
-add_loader( function() { $('previewbox').style.backgroundImage = $('srcImg').src; });
+add_loader( function() { $('previewbox').style.backgroundImage = 'url('+$('srcImg').src+')'; });
+var _MOUSE_X,_MOUSE_Y,originTop,originLeft;
 
