191aeecac09b43d403f416e47d334a3acba39281
Peter Law <PeterJCLaw@gmail.com>
2009-06-03 00:41:55 +0000

    updated BeSquidgy theme add a root function to the theme module block remove the theme root output from the site basehref block method
    git-svn-id: file:///media/Data/Repos/SquidgyCMS/trunk@321 2fba2af6-d892-294d-8f6b-3ba2989aa45f

--- Modules/Themes.module.php
+++ Modules/Themes.module.php
@@ -54,11 +54,11 @@ class BlockThemes extends Block {
 	function BlockThemes() {
 		parent::__construct();
 	}
-	function block($args) {
-
-		if(!is_readable($this->data_file))
-			return '<span id="themesblah" style="display: none;"> (The file was not readable)</span>';
-
+	function root($args) {
+		$site_theme = Themes::get_site_theme();
+		list($package, $theme) = explode('|', $site_theme, 2);
+		$root = "Themes/";
+		return '[[Block::Site-BaseHREF]]'.($package == 'Custom' ? 'Sites/Custom_'.$root : $root);
 	}
 }
 
