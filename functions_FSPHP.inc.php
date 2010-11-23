eb7bc96ac9f7ee1267f8a3e8cbc83d4ff64d4d66
Peter Law <PeterJCLaw@gmail.com>
2010-11-23 21:25:44 +0000

    remove pointless php tags

--- functions_FSPHP.inc.php
+++ functions_FSPHP.inc.php
@@ -17,8 +17,7 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
  */
-?>
-<?php
+
 class FSPHP{
 //Chris' orignial functions, which may have been tweaked by me, just a little
 
--- resize.php
+++ resize.php
@@ -16,8 +16,7 @@
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-?>
-<?php
+
 include_once("Global.inc.php");	//tweaked by Peter Law due to move to a single global include
 
 /* This function appends a string passed to the filename given */
