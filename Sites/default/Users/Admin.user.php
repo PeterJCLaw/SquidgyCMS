5258a33c48456e72c64b2aec3f789e8c43bddcee
Peter Law <PeterJCLaw@gmail.com>
2010-11-23 20:04:24 +0000

    Spelling fix

--- Sites/default/Users/Admin.user.php
+++ Sites/default/Users/Admin.user.php
@@ -8,7 +8,7 @@ $gender	= 'them';
 
 $spiel	= 'Use this space to talk about yourself.<br />This may well be made public.';
 
-$name	= 'Your name, as you would likke it displayed';
+$name	= 'Your name, as you would like it displayed';
 
 $auth_level = USER_ADMIN;
 
