ccf7240c3930ec054c6096517a77f5f0aa3ab90f
Peter Law <PeterJCLaw@gmail.com>
2011-05-09 22:56:00 +0100

    Add the beginnings of a completely new Files module.
    This files module is intended to repalce the current Docs module, which
    is so full of spaghetti it's insane. I'm hoping that I can build it in
    such a way that adding functionality by subclassing it will be easy.
    
    I'm planning to build the new Gallery module in this manner. The
    alternative might be to have this module support some form of dynamic
    templating.
    
    I've added a selection of icons from the Oxygen set
    (http://www.oxygen-icons.org/), which are freely (and I believe suitably)
    licensed, though this probably needs review.

---
+++ src/Modules/Files.module.php
@@ -0,0 +1,270 @@
+<?php
+#name = Files
+#description = Completely rewritten replacement for Docs
+#package = Core - optional
+#type = content
+###
+
+/*
+class AdminFiles extends Admin {
+	function AdminFiles() {
+		parent::__construct();
+		$this->complex_data = true;
+		$this->data_key_column = 'id';
+		$this->get_data();
+	}
+
+	function printFormAdmin() {
+	}
+
+	function submit($content=0) {
+	}
+}
+//*/
+
+class BlockFiles extends Block {
+
+	var $sizes = array(128, 64);
+	var $size = 128;
+
+	var $pathOffset; // "Sites/$site/Files/$whateverTheySpecInThePage";
+
+	// TODO: do we even want to spec 'Files' here?
+	function BlockFiles() {
+		parent::__construct();
+		$this->pathOffset = "Sites/$GLOBALS[site]/Files";
+	}
+
+	function Listing($args)
+	{
+		if (!empty($args[0]))
+		{
+			$folder = $args[0];
+			$folder = Path::tidy($folder);
+			if ($folder[0] == '/' || strpos($folder, '../') != False)
+			{
+				log_error('BlockFiles->Listing : Invalid path', array('path' => $path, 'args' => $args));
+				return False;
+			}
+
+			$this->pathOffset = "$this->pathOffset/$folder";
+		}
+
+		// TODO: actually getting $dir from the url or $_GET
+		$dir = '.';
+		$files = Folder::scan("$this->pathOffset/$dir");
+
+		$out = '<ul class="files-listing">';
+		foreach ($files as $file)
+		{
+			// TODO: filtering
+			$out .= $this->getTemplate("$dir/$file");
+		}
+		$out .= '</ul>';
+		return $out;
+
+	}
+
+	/**
+	 * Wraps the given item in the template for display.
+	 * @param relativePath The path to the item, relative to our localised filesystem root.
+	 */
+	function getTemplate($relativePath)
+	{
+		$itemName = basename($relativePath);
+		$itemName_wrap = wordwrap($itemName, 13, "<br />\n");
+		$link = $this->getLinkFor($relativePath);
+		$image = $this->getImageFor($itemName);
+		return <<<TPL
+<li>
+	<img src="$image" title="Click to download/view $itemName" />
+	<p>$itemName_wrap</p>
+</li>
+TPL;
+	}
+
+	/**
+	 * Returns the link to the item, be it a file or folder, adjusted for immediate use.
+	 */
+	function getLinkFor($itemName)
+	{
+		$link = "$this->pathOffset/$itemName";
+		if (is_dir($link))
+		{
+			// TODO: make this work, this is wrong
+			return $itemName;
+		}
+		return $link;
+	}
+
+	/**
+	 * Returns the type that this extension represents.
+	 * Several extensions are grouped together into one type,
+	 *  for instance jpg, jpeg and png are all images.
+	 */
+	function getIconTypeFor($ext)
+	{
+		if (empty($ext))
+		{
+			return 'folder-blue';
+		}
+
+		// TODO: does this want to be external, in a config file perhaps?
+		$typesMap = array(
+			'application-pdf'               => array('pdf'),
+			'application-vnd.ms-excel'      => array('xls', 'xlsx', 'ods'),
+			'application-vnd.ms-powerpoint' => array('ppt', 'pptx', 'odp'),
+			'application-vnd.ms-word'       => array('doc', 'docx', 'odt'),
+			'image-jpeg2000'                => array('jpg', 'jpeg', 'png', 'ico', 'gif'),
+			'audio-x-pn-realaudio-plugin'   => array('mp3', 'wma', 'aac'),
+			'text-plain'                    => array('txt', 'ini'),
+			'uri-mms'                       => array('avi', 'mkv', 'mp4', 'wmv'),
+			'text-xml'                      => array('xml', 'htm', 'html')
+		);
+
+		foreach ($typesMap as $type => $exts)
+		{
+			if (in_array($ext, $exts))
+			{
+				return $type;
+			}
+		}
+		return 'unknown';
+	}
+
+	/**
+	 * Returns the path to an icon for an extension type.
+	 * We look first for an icon for that type in the required size,
+	 *  then try other sizes, before defaulting to the 'Unknown' icon.
+	 */
+	function getIconFor($type)
+	{
+		// TODO: support other themes?
+		$base = "Modules/Files/Oxygen";
+		$file = "$base/$this->size/$type.png";
+
+		if (is_file($file))
+		{
+			return $file;
+		}
+
+		foreach ($this->sizes as $size)
+		{
+			$file = "$base/$size/$type.png";
+			if (is_file($file))
+			{
+				return $file;
+			}
+		}
+
+		if ($type != 'unknown')
+		{
+			return $this->getIconFor('unknown');
+		}
+
+		// should never get here...
+		return False;
+	}
+
+	/**
+	 * Returns the path to an image to display for a file.
+	 * We display an icon based on the type of the file.
+	 */
+	function getImageFor($name)
+	{
+		$ext = Path::getExtension($name);
+		$type = $this->getIconTypeFor($ext);
+		return $this->getIconFor($type);
+	}
+}
+
+/**
+ * A static class containing a collection of path related helper functions.
+ */
+class Path
+{
+	/**
+	 * Combine an arbitrary number of paths into one path, using '/' as a separator.
+	 */
+	function combine()
+	{
+		$parts = func_get_args();
+		$path = implode('/', $parts);
+		return self::tidy($path);
+	}
+
+	function getExtension($path)
+	{
+		$info = pathinfo($path);
+		return $info['extension'];
+	}
+
+	function getFileNameWithoutExtension($path)
+	{
+		$info = pathinfo($path);
+		$base = basename($path, $info['extension']);
+		return $base;
+	}
+
+	/**
+	 * Tidies a path, removing duplicate slashes etc.
+	 * Note that this is not the same as realpath,
+	 *  as the resulting path is still relative, and is not required to exist.
+	 */
+	function tidy($path)
+	{
+		while (strpos($path, '//') !== False)
+		{
+			$path = str_replace('//', '/', $path);
+		}
+
+		while (strpos($path, '../', 1) !== False)
+		{
+			$path = preg_replace('/[^\/]+\/\.\.\//', '', $path);
+		}
+
+		return $path;
+	}
+}
+
+/**
+ * A static class containing a collection of directory related helper functions.
+ */
+class Folder
+{
+	/**
+	 * Folder scanning function similar to PHP5 scandir, and uses that if available.
+	 * @param directory The directory that will be scanned.
+	 * @param directory By default, the sorted order is alphabetical in ascending order.
+	 *                  If the optional sorting_order is set to non-zero,
+	 *                   then the sort order is alphabetical in descending order.
+	 * @return Returns an array of filenames on success, or FALSE on failure.
+	 *         If directory is not a directory, then boolean FALSE is returned,
+	 *          and an error of level E_WARNING is generated.
+	 */
+	function scan($directory, $sorting_order = 0)
+	{
+		if (function_exists('scandir'))
+		{
+			return scandir($directory, $sorting_order);
+		}
+
+		$handle = opendir($directory);
+
+		if ($handle === False)
+		{
+			return False;
+		}
+
+		$files = array();
+
+		while ( ($filename = readdir($handle)) !== False )
+		{
+			$files[] = $filename;
+		}
+
+		closedir($handle);
+
+		return $files;
+	}
+}
