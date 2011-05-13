5d38f7a310fe6c087617b5a7bc9da9e2cab491ae
Peter Law <PeterJCLaw@gmail.com>
2011-05-14 00:14:01 +0100

    Add a GalleryImage class for resizing, partially do the ajax call.
    GalleryImage handles all the resizing for the image, and will also
    handle the cache management too. There's probably a case for this
    being a more general ResizableImage, so that it can be used elsewhere
    easily (Profile?).

--- src/Modules/Gallery.module.php
+++ src/Modules/Gallery.module.php
@@ -62,13 +62,17 @@ class BlockGallery extends BlockFiles {
 		$width = $height = $path = null;
 		extract($_GET, EXTR_IF_EXISTS);
 
-		if (!self::isFileTypeSupported($path))
-		{
-			die('File type not supported for resizing on the fly.');
-		}
+		$image = new GalleryImage($path);
+		$resource = $image->createResizedImage($width, $height);
 
-		// TODO: make this resize the image
-		Header('Location: Modules/Files/Oxygen/128/application-pdf.png');
+		// TODO: proper caching
+		$file = $this->cacheFolder.'/'.basename($path).'.jpg';
+		$ret = imagejpeg($resource, $file);
+		var_dump($ret);
+		// free memory
+		imagedestroy($resource);
+
+		Header("Location: $file");
 	}
 
 	/**
@@ -184,4 +188,136 @@ function toQueryString($query_data, $arg_separator = '&')
 
 	$query = implode($arg_separator, $pairs);
 	return $query;
+}
+
+/**
+ * Class akin to the FilesItem, but for images in the Gallery.
+ * As such it does not inherit from FilesItem, nor contain any of the templating functionality.
+ * Instead it is used to help resize the images.
+ * The only validation for image handling functionality is done in the constructor,
+ *  which will error if it detects an unsupported type.
+ */
+ // TODO: review -- should we inherit from FilesItem?
+ // TODO: review how we figure out which types are supported by using MIME types instead of file extensions.
+class GalleryImage
+{
+	var $path;
+	var $info;
+
+	function GalleryImage($path)
+	{
+		$this->path = $path;
+		$this->info = getimagesize($this->path);
+		if (!BlockGallery::isFileTypeSupported($this->path))
+		{
+			trigger_error('File type not supported for resizing on the fly', E_USER_ERROR);
+		}
+	}
+
+	/**
+	 * Creates a new image that is a copy of the current one,
+	 *  resized to fit in the specified box.
+	 * @param width The (maximum) width of the new image.
+	 * @param height The (maximum) height of the new image.
+	 * @param keepRatio Whether or not to preserve the current aspect ratio of the image.
+	 * @returns A handle to the new image, or null if it couldn't be created.
+	 */
+	function createResizedImage($width, $height, $keepRatio = True)
+	{
+		if ($keepRatio === True)
+		{
+			$newSize = $this->getSizeInBox($width, $height);
+			$height = $newSize['height'];
+			$width = $newSize['width'];
+		}
+
+		$newImage = imagecreatetruecolor($width, $height);
+		$curImage = $this->loadFromFile();
+
+		// TODO: do we need to imagedestroy these anywhere?
+
+		// can go no further if we don't have the resources.
+		if ($curImage === null || $newImage === null)
+		{
+			echo 'load or create fail';
+			return null;
+		}
+
+		$res = imagecopyresampled($newImage, $curImage, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+		if (!$res)
+		{
+			echo 'resize fail';
+			return null;
+		}
+		return $newImage;
+	}
+
+	/**
+	 * Convenience function that delegates to BlockGallery::fitInBox.
+	 * See that function for full details.
+	 */
+	function getSizeInBox($boxWidth, $boxHeight)
+	{
+		$w = $this->getWidth();
+		$h = $this->getHeight();
+		$ret = BlockGallery::fitInBox($boxWidth, $boxHeight, $w, $h);
+		return $ret;
+	}
+
+	/**
+	 * Load the image from the file.
+	 * Note that while this uses the file extension to determine the file type,
+	 *  no validation is done regarding the availability of the image handling functions.
+	 * @returns a handle to the resource of the image, or null if it could not be loaded.
+	 */
+	function loadFromFile()
+	{
+		$resource = null;
+		$mime = $this->getMIME();
+		if ($mime == 'image/png')
+		{
+			$resource = imagecreatefrompng($this->path);
+		}
+		elseif ($mime == 'image/gif')
+		{
+			$resource = imagecreatefromgif($this->path);
+		}
+		elseif ($mime == 'image/jpeg')
+		{
+			$resource = imagecreatefromjpeg($this->path);
+		}
+		return $resource;
+	}
+
+	/**
+	 * Get the MIME type of the image.
+	 */
+	function getMIME()
+	{
+		return $this->info['mime'];
+	}
+
+	/**
+	 * Get the height of the image, in pixels.
+	 */
+	function getHeight()
+	{
+		return $this->info[1];
+	}
+
+	/**
+	 * Get the width of the image, in pixels.
+	 */
+	function getWidth()
+	{
+		return $this->info[0];
+	}
+
+	/**
+	 * Get the md5 of the original image.
+	 */
+	function getMD5()
+	{
+		$md5 = md5_file($this->path);
+	}
 }
