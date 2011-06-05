db90ecad39686b25af10e99382fc63593ef87715
Peter Law <PeterJCLaw@gmail.com>
2011-06-05 09:54:41 +0100

    Convert Email's docs to doxygen style

--- src/Modules/Email.php
+++ src/Modules/Email.php
@@ -18,43 +18,59 @@ class Email {
 		$this->headers	= array();
 	}
 
-	/* add a recipient */
+	/**
+	 * Add a recipient.
+	 */
 	function add_to($email, $name=FALSE) {
 		$this->to[$email] = $name;
 	}
 
-	/* add a CC recipient */
+	/**
+	 * Add a CC recipient.
+	 */
 	function add_cc($email, $name=FALSE) {
 		$this->cc[$email] = $name;
 	}
 
-	/* add a BCC recipient */
+	/**
+	 * Add a BCC recipient.
+	 */
 	function add_bcc($email, $name=FALSE) {
 		$this->bcc[$email] = $name;
 	}
 
-	/* set a From address */
+	/**
+	 * Set a From address.
+	 */
 	function set_from($email, $name=FALSE) {
 		$this->from = array();	//only allow one from address, so clear any previous ones
 		$this->from[$email] = $name;
 	}
 
-	/* add a single header line */
+	/**
+	 * Add a single header line.
+	 */
 	function add_header($header) {
 		$this->headers[] = trim($header);
 	}
 
-	/* set the subject */
+	/**
+	 * Set the subject.
+	 */
 	function set_subject($subject) {
 		$this->subject = $subject;
 	}
 
-	/* set the message body */
+	/**
+	 * Set the message body.
+	 */
 	function set_body($body) {
 		$this->body = $body;
 	}
 
-	/* convert an array of addresses into a comma separated string ready for sending */
+	/**
+	 * Convert an array of addresses into a comma separated string ready for sending.
+	 */
 	function addresses_to_string($arr) {
 		foreach($arr as $email => $name) {
 			$list[] = (empty($name) ? '' : "$name ")."<$email>";
@@ -62,7 +78,9 @@ class Email {
 		return implode(', ', $list);
 	}
 
-	/* actually send the email */
+	/**
+	 * Actually send the email.
+	 */
 	function send() {
 		$to = $this->addresses_to_string($this->to);
 
@@ -88,12 +106,16 @@ class EmailLink extends Email {
 		parent::__construct();
 	}
 
-	/* convert an array of addresses into a comma separated string ready for using in a mailto link */
+	/**
+	 * Convert an array of addresses into a comma separated string ready for using in a mailto link.
+	 */
 	function addresses_to_string($arr) {
 		return implode(',', array_keys($arr));
 	}
 
-	/* generate a mailto link */
+	/**
+	 * Generate a mailto link.
+	 */
 	function link($full=FALSE, $text='', $opts=array()) {
 		foreach(array('cc', 'bcc', 'subject', 'body') as $name) {
 			if(empty($this->$name))
